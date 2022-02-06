86898f7ce92930576d7a0865838f07ac4918b13a
Georgina Bada <georginabada@gmail.com>
2022-02-06 17:52:25 -0500

    implementing User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
-
+  # about page 
+  resources :about, only: [:index]
   resource :cart, only: [:show] do
     post   :add_item
     post   :remove_item
@@ -17,8 +18,7 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
-   # about page
-resources :about, only: [:show]
+ 
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
